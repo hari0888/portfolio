2f06db1ee344f7528f65c4aedba058abc413c708
hari <106533496+hari0888@users.noreply.github.com>
2026-03-03 10:45:40 -0600

    Update LinkedIn link

--- src/portfolio.js
+++ src/portfolio.js
@@ -48,7 +48,7 @@ const socialMediaLinks = [
   },
   {
     name: "LinkedIn",
-    link: "https://www.linkedin.com/in/hari-kumar-reddy-yennam-457a9620a/",
+    link: "https://www.linkedin.com/in/hari-full-stack-developer",
     fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
     backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
   },
